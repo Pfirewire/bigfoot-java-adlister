a168169db99e7fb8639c399754d5c92bcde03d5e
Steve Pflug <stephen@pflug.us>
2023-05-10 09:08:06 -0500

    completed MVC Accessing Data exercises

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -4,16 +4,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet("/ads")
+public class AdsServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Ads adsDao = DaoFactory.getAdsDao();
+        List<Ad> ads = adsDao.all();
+
+        req.setAttribute("ads", ads);
+        req.getRequestDispatcher("/ads/index.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/profile.jsp").forward(req, resp);
+
     }
 }
